a55c0c111728f17e0251f6569853fe5ad9dfac45
tkser <tkser.edu@gmail.com>
2023-07-20 01:41:42 +0900

    fix: name

--- src/components/Name.tsx
+++ src/components/Name.tsx
@@ -1,17 +1,14 @@
-import { Grid, Stack, Typography, useMediaQuery } from "@mui/material"
+import { Avatar, Grid, Stack, Typography, useMediaQuery } from "@mui/material"
 
 const Name = () => {
   const isWideScreen = useMediaQuery('(min-width: 950px)');
   return (
-    <Grid container alignItems={"center"} justifyContent={"center"} direction={"column"} style={{ height: '100%' }}>
-      <Grid item sx={{width: "min-content"}}>
-        <Typography variant="h1" component="h1" sx={{display: "inline", whiteSpace: isWideScreen ? "inherit" : "nowrap"}}>
-          Shu Takahashi
-        </Typography>
-        <Stack direction="row" spacing={2} sx={{display: "inline-flex"}}>
-            <Typography variant="h4" component="p">tkser</Typography>
-            <Typography variant="h4" component="p">tksnn</Typography>
-          </Stack>
+    <Grid container alignItems={"center"} justifyContent={"center"} direction={"column"} style={{ height: '100%' }} marginTop={1} marginBottom={1}>
+      <Grid item sx={{ width: "100%" }}>
+        <Stack direction={isWideScreen ? "column" : "row"} justifyContent={isWideScreen ? "center" : "flex-start"} alignItems={"center"} gap={3} marginLeft={isWideScreen ? "inherit" : 2} >
+          <Avatar sx={isWideScreen ? { width: "50%", maxWidth: "250px", aspectRatio: "1/1", height: "auto" } : {}} src="https://avatars.githubusercontent.com/u/46104508" />
+          <Typography variant={isWideScreen ? "h3" : "h4"} component="h1">tksnn</Typography>
+        </Stack>
       </Grid>
     </Grid>
   )
